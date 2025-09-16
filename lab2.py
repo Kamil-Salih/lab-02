ed0ae36908643e537bff675627270b2b8e4a6a29
Kamil-Salih <C00307549@setu.ie>
2025-09-16 11:27:15 +0000

    update

--- lab2.py
+++ lab2.py
@@ -3,8 +3,7 @@ Step 1: Print the file lines
 Write a Python script (lab2.py) that opens auth.log and prints each line.
 
 Step 2: Extract IP addresses
-Use the regex r"\d+\.\d+\.\d+\.\d+" to find all IP addresses.
-create a list 
+Use the regex r"\d+\.\d+\.\d+\.\d+" to find all IP addresses. Create a list 
 
 Step 3: Get unique IPs
 Convert the list to a set, then print each unique IP.
@@ -12,3 +11,15 @@ Convert the list to a set, then print each unique IP.
 Step 4: Save results
 Write the unique IPs to a new file unique_ips.txt, one per line.
 """
+import re
+
+pattern= r"\d+\.\d+\.\d+\.\d+"
+ips = []     # creates an empty list called ips
+
+with open('auth.log', 'r') as f:
+    for line in f:
+        print(line.strip())
+
+print()
+
+
